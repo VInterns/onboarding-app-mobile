b006c6b5bcade2bdf0d906871b2d656665eaf0be
Hagar <hagar.aboelfottouh1@vodafone.com>
2019-09-30 14:34:44 +0200

    commit 3

--- App/component/HealthAndSafety11.js
+++ App/component/HealthAndSafety11.js
@@ -1,113 +1,97 @@
 import React, { Component } from 'react';
+
 import {
     StyleSheet,
     View,
-    TouchableOpacity,
     Image,
     Dimensions,
-    Text
+    Text,
 } from 'react-native';
 
-import D from '../assets/D.png';
+import img1 from '../assets/D.png';
 
 export default class HealthAndSafety11 extends Component {
-    static navigationOptions = {//header styling
+    componentDidMount() {
+        this.timeoutHandle = setTimeout(() => {
+            this.props.navigation.navigate("HealthAndSafety")
+        }, 2000);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timeoutHandle);
+    }
+
+    static navigationOptions = {
         header: null
     };
+
     render() {
-        const { navigate } = this.props.navigation;
         return (
-            <View>
-                <View style={{ width: '100%', height: '100%', flex: 6, alignItems: "center", flexDirection: "column" }}>
-
-                    <View style={styles.TextContainer}>
-                        <Text style={styles.redText}>
-                            Congratulations!
-                </Text>
-                        <Text style={styles.blackText}>
-                            You’ve been awarded with the letter
-                </Text>
-                    </View>
-                    <Image source={D} style={styles.Letter}>
-                    </Image>
-
+            <View style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text1}>
+                        Congratulations!
+                    </Text>
+                    <Text style={styles.text2}>You’ve been awarded with the letter</Text>
                 </View>
-                <View style={{ flex: 1 }}>
-                    <View style={styles.ButtonsView}>
-                        <TouchableOpacity style={styles.BackButton} onPress={() => navigate("HealthAndSafety10", {})} >
-                            <Text style={styles.ButtonText}>
-                                BACK
-                 </Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={styles.NextButton} onPress={() => navigate("HealthAndSafety11", {})} >
-                            <Text style={styles.ButtonText}>
-                                NEXT
-                 </Text>
-                        </TouchableOpacity>
-                    </View>
+                <View style={styles.imageContainer}>
+                    <Image source={img1} style={styles.img} />
                 </View>
-            </View>
+            </View >
         );
     }
 }
-
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
-
 const styles = StyleSheet.create({
-    TextContainer:
-    {
-        // flex: 1
+
+    container: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        flex: 1,
     },
-    Letter:
-    {
-        // marginLeft: DEVICE_WIDTH / 2 - 107,
-        marginTop: DEVICE_HEIGHT / 3.2,
-        // flex: 2
+    textContainer: {
+        flex: 1,
+        padding: DEVICE_WIDTH * 0.05,
+        marginTop: DEVICE_HEIGHT * 0.1,
     },
-    blackText: {
-        color: 'black',
-        fontSize: 15,
+    text1: {
+        fontSize: 37,
+        fontWeight: "bold",
+        fontStyle: "normal",
+        lineHeight: 44,
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#e60000"
     },
-    redText: {
-        color: 'red',
-        fontSize: 28,
-        fontWeight: 'bold',
-        marginTop: DEVICE_HEIGHT / 6
+    text2: {
+        fontSize: 21,
+        fontWeight: "normal",
+        fontStyle: "normal",
+        letterSpacing: 0,
+        textAlign: "center",
+        color: "#4b464d"
     },
-    ButtonsView: {
-        // width: DEVICE_WIDTH,
-        height: 45,
-        flexDirection: 'row',
-        marginTop: DEVICE_HEIGHT * 0.93,
-        // marginBottom: DEVICE_HEIGHT * 0.03,
-        backgroundColor: "#0000",
-        shadowColor: 'black',
-        shadowOffset: {
-            width: 10,
-            height: 10
-        },
-        shadowRadius: 22,
-        shadowOpacity: 1,
-        elevation: 3,
-        // position: "absolute"
-    },
-    BackButton: {
+    imageContainer: {
+        flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        marginLeft: DEVICE_WIDTH * 0.11
+        flex: 6,
+        marginTop: DEVICE_HEIGHT * 0.04,
     },
-    NextButton: {
+    button: {
         alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        marginLeft: DEVICE_WIDTH * 0.5
+        justifyContent: 'flex-end',
+    },
+    NextButtonView: {
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        marginBottom: DEVICE_HEIGHT * 0.03,
+        marginRight: DEVICE_WIDTH * 0.095,
     },
     ButtonText: {
         color: 'black',
         fontWeight: 'bold',
         fontSize: 20,
-    }
+    },
 });
