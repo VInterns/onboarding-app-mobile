b5ba33ec0d43f1eb070121ab8d6cbcb94de94521
Mohamed AlMokadem <Mohamed.AlMokadem@vodafone.com>
2019-10-11 13:36:58 +0200

    adding text to screen

--- App/component/HealthandSafety2.js
+++ App/component/HealthandSafety2.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
 import Draggable from "./Draggable";
+
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Text,
+} from 'react-native';
 
 
 // import { centerImg } from '../assets/undrawConnectingTeams8Ntu.png';
@@ -30,6 +37,10 @@ export default class HealthandSafety2 extends Component {
     this.state = {};
     // this.q1true = this.q1true.bind(this);
 }
+static navigationOptions = {
+  //header styling
+  header: null
+};
 
   props: {
     imageurl: any,
@@ -41,13 +52,32 @@ export default class HealthandSafety2 extends Component {
     debugger;
     return (
       <View style={styles.mainContainer}>
-        <View style={styles.dropZone}>
-          {/* <Text style={styles.text}>Drop them here!</Text> */}
+        <View style={styles.titleContainer}>
+          <Text style={styles.textlarge}>Match  
+            <Text style={styles.text}> Match the image with the matching text :</Text>
+          </Text>
         </View>
-        <View style={styles.ballContainer} />
-        <View style={styles.row}>
-          <Draggable imageurl={centerImg} moveY={120} moveX={40} answerId={1} qtrue={q1true}/>
-          <Draggable imageurl={centerImg} moveY={140} moveX={80} answerId={2} qtrue={q2true}/>
+        <View style={styles.imagesContainer}>
+          {/* images here  */}
+          <View style={styles.dropZone}>
+            {/* <Text style={styles.text}>Drop them here!</Text> */}
+          </View>
+          <View style={styles.dropZone}>
+            {/* <Text style={styles.text}>Drop them here!</Text> */}
+          </View>
+          <View style={styles.dropZone}>
+            {/* <Text style={styles.text}>Drop them here!</Text> */}
+          </View>
+          <View style={styles.dropZone}>
+            {/* <Text style={styles.text}>Drop them here!</Text> */}
+          </View>
+        </View>
+        <View style={styles.answersContainer}>
+          {/* answers here */}
+          
+
+          {/* <Draggable imageurl={centerImg} moveY={120} moveX={40} answerId={1} qtrue={q1true}/> */}
+          {/* <Draggable imageurl={centerImg} moveY={140} moveX={80} answerId={2} qtrue={q2true}/> */}
         </View>
       </View>
     );
@@ -55,31 +85,72 @@ export default class HealthandSafety2 extends Component {
 }
 
 let CIRCLE_RADIUS = 30;
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    padding:36,
+    backgroundColor:'#EE82EE',
+        
   },
   ballContainer: {
-    height:200
+    // height:200
   },
   row: {
-    flexDirection: "row",
-    backgroundColor: "yellow",
+    // flexDirection: "row",
   },  
   dropZone: {
-    height: 200,
+    // height: 200,
     backgroundColor: "#00334d",
     width: CIRCLE_RADIUS * 2,
     height: CIRCLE_RADIUS * 2,
     borderRadius: CIRCLE_RADIUS,
   },
-  text: {
-    marginTop: 25,
-    marginLeft: 5,
-    marginRight: 5,
-    textAlign: "center",
-    color: "#fff",
-    fontSize: 25,
-    fontWeight: "bold"
-  }
+  titleContainer:{
+    flexDirection: 'row',
+    backgroundColor: '#00BFFF',
+    flex: 2,
+    paddingTop: DEVICE_HEIGHT * 0.03,
+    flexWrap: 'wrap',
+  },
+  textlarge:{
+    fontSize: 28,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 25,
+    letterSpacing: 0,
+    color: "#e60000",
+    flexWrap: 'wrap',
+  },
+  text:{
+    fontSize: 21,
+    fontWeight: "bold",
+    fontStyle: "normal",
+    lineHeight: 25,
+    letterSpacing: 0,
+    color: "#4c464e",
+    flexWrap: 'wrap',
+  },
+  imagesContainer:{
+    backgroundColor: 'yellow',
+    flex :2,
+    flexDirection: 'row',
+    justifyContent: 'space-between' 
+
+    // padding:10,
+  },
+  answersContainer:{
+    backgroundColor: 'orange',
+    flex:6,
+    padding:36
+  },
+
+  
+
+  
 });
--- App/services/http-client/constants.js
+++ App/services/http-client/constants.js
@@ -1,7 +1,7 @@
 const PROD_BASE_URL = "";
 
 // const DEV_BASE_URL = "https://66b0bf91.ngrok.io/";
-const DEV_BASE_URL = "https://a9b0dcac.ngrok.io/";
+const DEV_BASE_URL = "https://c61c54a7.ngrok.io/";
 
 // const PROD_BASE_URL = "";
 const DEV = true;
