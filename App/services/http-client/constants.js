a5b83f99d12394484ade91a3a86a32776f0f88e3
Hagar <hagar.aboelfottouh1@vodafone.com>
2019-10-19 19:10:35 +0200

    ngrok

--- App/services/http-client/constants.js
+++ App/services/http-client/constants.js
@@ -2,7 +2,7 @@ const PROD_BASE_URL = "";
 
 // const DEV_BASE_URL = "https://66b0bf91.ngrok.io/";
 
-const DEV_BASE_URL = "https://98818b26.ngrok.io/";
+const DEV_BASE_URL = "https://024d1ded.ngrok.io/";
 
 
 
