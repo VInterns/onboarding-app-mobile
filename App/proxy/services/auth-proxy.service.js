25e5f3f6293ddecad8f968601c6a78d4f5f63b57
Hagar <hagar.aboelfottouh1@vodafone.com>
2019-11-11 12:15:19 +0200

    Fix the issue of returned bodyblob instead of body

--- App/proxy/services/auth-proxy.service.js
+++ App/proxy/services/auth-proxy.service.js
@@ -4,6 +4,7 @@ import {
   UserLoginModel,
   UserConfirmModel
 } from "../models/index";
+var userIdVar = ""
 
 export class AuthProxyService {
 
@@ -18,22 +19,34 @@ export class AuthProxyService {
     data["email"] = user.email;
     data["password"] = user.password;
 
+    // var res = await fetch(`${BASE_URL}api/account/login`, {
+    //   method: "post",
+    //   headers: { "content-Type": "application/json" },
+    //   body: JSON.stringify(data)
+    // }).then(response => response.json()).then(responseJson => {
+    //   // this.setState({
+    //   //   userId: responseJson.userId
+    //   // }, function () {
+    //   //   console.log("inside the function : ", this.state.userId);
+
+    //   // });
+    //   userIdVar = responseJson;
+    //   console.log("var : ", userIdVar);
+    // }
+    // );
+    // debugger;
+    // // let response = {};
+    // // response["status"] = res.status;
+    // // response["userId"] = userIdVar;
+    // return userIdVar;
+
+
+
     return await fetch(`${BASE_URL}api/account/login`, {
       method: "post",
       headers: { "content-Type": "application/json" },
       body: JSON.stringify(data)
-    });
-
-
-    // await fetch(`${BASE_URL}api/account/login`, {
-    //   method: "post",
-    //   headers: { "content-Type": "application/json" },
-    //   body: JSON.stringify(data)
-    // }).then(response => response.json()).then((data) => {
-    //   this.setState({ userId: data })
-    // });
-
-    // return this.state.userId;
+    }).then(response => { return response.json() });
   }
 
   async register(user: UserRegisterModel) {
